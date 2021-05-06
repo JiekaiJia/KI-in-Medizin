Fixed Totensor Class floattensor bug. Fixed Rescale invalid bug. Added regularization l2 to the loss function. Added batch normalization to the CNN model.

data_preprocessing.py
@@ -97,7 +97,7 @@ def cut_signal(num_signals, data_df, wished_length):
 class EcgDataset(Dataset):
     """ECG dataset."""
 
-    def __init__(self, csv_file, root_dir, transform=None, normalize=True):
+    def __init__(self, csv_file, root_dir, transform=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -105,11 +105,9 @@ class EcgDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
             normalize(bool): Optional normalization to be applied on a sample.
         """
-        self.normalize = normalize
         self.ecg_frame = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
-
         self.classes = ['N', 'A', 'O', '~']
 
     def __len__(self):
@@ -125,10 +123,6 @@ class EcgDataset(Dataset):
         signal = signal.reshape(1, -1)
         label = self.ecg_frame.iloc[idx, 1]
         target = self.classes.index(label)
-
-        if self.normalize:
-            # Normalize the signal
-            signal = preprocessing.normalize(signal, norm='l2')
 
         if self.transform:
             signal = self.transform(signal)
@@ -152,7 +146,7 @@ class Rescale(object):
     def __call__(self, signal):
 
         h = 1
-        w = signal.shape[0]
+        w = signal.shape[1]
         if isinstance(self.output_size, int):
             new_h = h
             new_w = self.output_size
@@ -161,7 +155,7 @@ class Rescale(object):
 
         if w < new_w:
             # If w is smaller, padding w with 0 util new_w
-            np.pad(signal, (0, new_w-w), 'constant', constant_values=(0, 0))
+            signal = np.pad(signal, ((0, 0), (0, new_w-w)), 'constant', constant_values=(0, 0))
         elif w == new_w:
             pass
         else:
@@ -178,7 +172,8 @@ class ToTensor(object):
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
-        return torch.from_numpy(signal)
+        signal = torch.from_numpy(signal)
+        return signal.float()
 
 # display_df(pre_data_df, random=True, layers=4)
 # data_df.to_csv('raw_data.csv', sep=' ', index=False)  # save the raw data as .csv file

main.py
@@ -41,10 +41,9 @@ if __name__ == '__main__':
         csv_file='../data/raw_data/training/REFERENCE.csv',
         root_dir='../data/raw_data/training',
         transform=composed,
-        normalize=True
     )
     # Split dataset into train dataset and test dataset
-    train_loader, vld_loader = get_data_loader(dataset, 8)
+    train_loader, vld_loader = get_data_loader(dataset, 32)
     # Get the default device 'cpu' or 'cuda'
     device = get_default_device()
     # Create data loader iterator
@@ -55,23 +54,25 @@ if __name__ == '__main__':
     # Transmit the model to the default device
     to_device(model, device)
 
-    num_epochs = 10
+    num_epochs = 40
     opt_fn = torch.optim.Adam
-    lr = 1e-5
+    lr = 1e-4
     # Train the model
-    train_losses, vld_losses, metrics = fit(
+    train_losses, train_metrics, vld_losses, vld_metrics = fit(
         num_epochs, lr, model, F.cross_entropy,
         train_loader, vld_loader, accuracy, opt_fn
     )
 
     vld_loss, total, vld_acc = evaluate(model, F.cross_entropy, vld_loader, metric=accuracy)
+    _, _, train_acc = evaluate(model, F.cross_entropy, train_loader, metric=accuracy)
     print(f'Loss: {vld_loss:.4f}, Accuracy: {vld_acc:.4f}')
 
     # Replace these values with your results
-    accuracies = [vld_acc] + metrics
-    plt.plot(accuracies, '-x')
+    vld_accuracies = vld_metrics
+    train_accuracies = train_metrics
+    plt.plot(vld_accuracies, '-x')
+    plt.plot(train_accuracies, '-o')
     plt.xlabel('epoch')
     plt.ylabel('accuracy')
     plt.title('Accuracy vs. No. of epochs')
     plt.show()
-    print(dataset['label'].unique())

models.py
@@ -13,28 +13,35 @@ class CnnBaseline(nn.Module):
         # input 18286
         self.network = nn.Sequential(
             nn.Conv1d(in_channels=1, out_channels=16, kernel_size=64, stride=2, padding=32),  # 9144
-            nn.ReLU(),
+            # nn.BatchNorm1d(16),
+            nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=8),  # 1143
 
             nn.Conv1d(in_channels=16, out_channels=32, kernel_size=33, stride=2, padding=16),  # 573
-            nn.ReLU(),
+            # nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
 
             nn.Conv1d(in_channels=32, out_channels=64, kernel_size=17, stride=2, padding=8),  # 288
-            nn.ReLU(),
+            # nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
 
             nn.Conv1d(in_channels=64, out_channels=128, kernel_size=8, stride=2, padding=4),  # 145
-            nn.ReLU(),
+            # nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=5),  # 29
 
             nn.Conv1d(in_channels=128, out_channels=256, kernel_size=5, stride=2, padding=2),  # 16
-            nn.ReLU(),
+            # nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
 
             nn.Conv1d(in_channels=256, out_channels=512, kernel_size=2, stride=1, padding=1),  # 16
-            nn.ReLU(),
+            # nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=4),  # 4
 
             nn.Conv1d(in_channels=512, out_channels=1024, kernel_size=2, stride=1, padding=1),  # 4
-            nn.ReLU(),
+            # nn.BatchNorm1d(1024),
+            nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=4),  # 4
 
             nn.Flatten(),

utils.py
@@ -31,7 +31,7 @@ def split_indices(n, vld_pct, random_state=None):
 
 def get_data_loader(data_set, batch_size):
     """This function generate the batch data for every epoch."""
-    train_indices, vld_indices = split_indices(len(data_set), 0.9, random_state=2021)
+    train_indices, vld_indices = split_indices(len(data_set), 0.2, random_state=2021)
     train_sampler = SubsetRandomSampler(train_indices)
     train_ld = DataLoader(data_set, batch_size, sampler=train_sampler)
     vld_sampler = SubsetRandomSampler(vld_indices)
@@ -105,16 +105,17 @@ def evaluate(model, loss_func, valid_dl, metric=None):
 
 def fit(epochs, lr, model, loss_func, train_dl, vld_dl, metric=None, opt=None):
     """"""
-    train_losses, vld_losses, vld_metrics = [], [], []
+    train_losses, train_metrics, vld_losses, vld_metrics = [], [], [], []
     if opt is None:
         opt = torch.optim.SGD
 
-    opt = opt(model.parameters(), lr=lr)
+    opt = opt(model.parameters(), lr=lr, weight_decay=0.05)
     for epoch in range(epochs):
         train_loss = 0
+        train_metric = 0
         model.train()
         for xb, yb in train_dl:  # Training
-            train_loss, _, _ = loss_batch(model, loss_func, xb, yb, opt)
+            train_loss, _, train_metric = loss_batch(model, loss_func, xb, yb, opt, metric)
 
         model.eval()
         result = evaluate(model, loss_func, vld_dl, metric)  # Evaluation
@@ -122,15 +123,16 @@ def fit(epochs, lr, model, loss_func, train_dl, vld_dl, metric=None, opt=None):
         vld_losses.append(vld_loss)  # Record the loss & metric
         vld_metrics.append(vld_metric)
         train_losses.append(train_loss)
+        train_metrics.append(train_metric)
 
         # print progress
         if metric is None:
             print(f'Epoch [{epoch+1}/{epochs}], train_loss: {train_loss:.4f}, validation_loss: {vld_loss:.4f}')
         else:
             print(f'Epoch [{epoch+1}/{epochs}], train_loss: {train_loss:.4f}, validation_loss: {vld_loss:.4f}, '
-                  f'{metric.__name__}: {vld_metric:.4f}')
+                  f'validation {metric.__name__}: {vld_metric:.4f}, train {metric.__name__}: {train_metric:.4f}')
 
-    return train_losses, vld_losses, vld_metrics
+    return train_losses, train_metrics, vld_losses, vld_metrics
 
 
 def accuracy(outputs, labels):